3a7296cf81c3207b138611cbed2d2bd0271d26ef
Akram Ata <akramatm012@gmail.com>
2025-10-02 01:41:20 +0300

    - added blocProvider

--- bloc_app/lib/app_router.dart
+++ bloc_app/lib/app_router.dart
@@ -1,14 +1,31 @@
+import 'package:bloc_app/business_logic/cubit/characters_cubit.dart';
 import 'package:bloc_app/constants/strings.dart';
+import 'package:bloc_app/data/apis/characters_api_services.dart';
+import 'package:bloc_app/data/repo/characters_repo.dart';
 import 'package:bloc_app/presentation/screens/character_details.dart';
 import 'package:bloc_app/presentation/screens/characters_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppRouter {
+  late CharactersRepo charactersRepo;
+  late CharactersCubit charactersCubit;
+
+  AppRouter() {
+    charactersRepo = CharactersRepo(CharactersApiServices());
+    charactersCubit = CharactersCubit(charactersRepo);
+  }
+
   Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case charactersScreen:
-        return MaterialPageRoute(builder: (_) => CharactersScreen());
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => charactersCubit,
+            child: CharactersScreen(),
+          ),
+        );
+        
       case characterDetailsScreen:
         return MaterialPageRoute(builder: (_) => CharacterDetailsScreen());
     }
